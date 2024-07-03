788720506873129d0e5765aa04a3d77858077b37
Soila Pertet <nicolepertet@gmail.com>
2024-07-03 14:49:33 -0600

    Initialise a methods to connect to our MongoDB database

--- src/main/java/com/PointOfSaleSystem/StoreDatabase/StoreDatabase.java
+++ src/main/java/com/PointOfSaleSystem/StoreDatabase/StoreDatabase.java
@@ -12,9 +12,12 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+// Singleton Design pattern
 public class StoreDatabase {
 
     // Initialise instance variables
+    private static StoreDatabase storeDB;
+
     private List storeEmployees = new ArrayList();
     private List storeManagers = new ArrayList();
     private List softGoodsAssociates = new ArrayList();
@@ -22,5 +25,29 @@ public class StoreDatabase {
     private List storeCashiers = new ArrayList();
     private List ecomAssociates = new ArrayList();
 
+    private StoreDatabase() { };
+
+    public static StoreDatabase getInstance() {
+
+        if(storeDB == null) {
+            storeDB = new StoreDatabase();
+        }
+
+        return storeDB;
+    }
+
+    // Initialise a method to connect to our MongoDB database
+    private void connectToDatabase() {
+        String connectionString = "mongodb+srv://nicolepertet:vinsmoke.20.07@pos-cluster.wy7nnbe.mongodb.net/?retryWrites=true&w=majority&appName=POS-Cluster";
+
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(connectionString))
+                .serverApi(serverApi)
+                .build();
+    }
 }
 
