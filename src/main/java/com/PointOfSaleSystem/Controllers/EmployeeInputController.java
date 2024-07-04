fe08dd52085d92cd661a6a7f842e063c068d8255
Soila Pertet <nicolepertet@gmail.com>
2024-07-04 13:37:51 -0600

    Initialise EmployeeInputController class

---
+++ src/main/java/com/PointOfSaleSystem/Controllers/EmployeeInputController.java
@@ -0,0 +1,10 @@
+package com.PointOfSaleSystem.Controllers;
+
+public class EmployeeInputController {
+
+    private int employeeID;
+    private String loginPassword;
+    private boolean clockedIn;
+
+    public EmployeeInputController() { }
+}
