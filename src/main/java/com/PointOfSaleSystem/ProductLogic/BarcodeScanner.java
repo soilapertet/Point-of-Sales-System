7c4a4def6b489ab17c0c40a3b507a504eb83bf87
Soila Pertet <nicolepertet@gmail.com>
2024-07-18 10:26:12 -0600

    Initialise BarcodeScanner class

---
+++ src/main/java/com/PointOfSaleSystem/ProductLogic/BarcodeScanner.java
@@ -0,0 +1,16 @@
+package com.PointOfSaleSystem.ProductLogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BarcodeScanner {
+
+    // Define the instance variables
+    private List<BarcodedProduct> scannedProducts;
+    private double totalPrice;
+    private double subtotalPrice;
+
+    // Define the class constructor
+    public BarcodeScanner() { }
+
+}
