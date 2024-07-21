76fcf0cc065432f59d382a1f885d780588998b39
Soila Pertet <nicolepertet@gmail.com>
2024-07-21 11:52:35 -0600

    Define functionality to get the scanned upc of the product

--- src/main/java/com/PointOfSaleSystem/ProductLogic/BarcodeScanner.java
+++ src/main/java/com/PointOfSaleSystem/ProductLogic/BarcodeScanner.java
@@ -2,15 +2,33 @@ package com.PointOfSaleSystem.ProductLogic;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BarcodeScanner {
 
     // Define the instance variables
     private List<BarcodedProduct> scannedProducts;
+    private BarcodedProduct scannedProduct;
     private double totalPrice;
     private double subtotalPrice;
+    private long scannedUPC;
 
     // Define the class constructor
     public BarcodeScanner() { }
 
+    // Scan the upc on the product
+    public void scanBarcodeProduct() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Scan item here: ");
+        scannedUPC = scanner.nextLong();
+    }
+
+    // Define getter method
+    public long getScannedUPC() { return scannedUPC; }
+
+    public static void main(String[] args) {
+        BarcodeScanner barcodeScanner = new BarcodeScanner();
+        barcodeScanner.scanBarcodeProduct();
+        System.out.println(barcodeScanner.getScannedUPC());
+    }
 }
