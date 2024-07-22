e69b1b84416865c8e76d6ce4ee8324dee114ac17
Soila Pertet <nicolepertet@gmail.com>
2024-07-22 10:06:13 -0600

    Add method to calculate the total price of the products with taxes

--- src/main/java/com/PointOfSaleSystem/Controllers/ScanProductsController.java
+++ src/main/java/com/PointOfSaleSystem/Controllers/ScanProductsController.java
@@ -3,6 +3,7 @@ package com.PointOfSaleSystem.Controllers;
 import com.PointOfSaleSystem.ProductLogic.BarcodedProduct;
 import com.PointOfSaleSystem.StoreDatabase.InventoryDatabase;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,12 +17,18 @@ public class ScanProductsController {
     private BarcodedProduct barcodedProduct;
     private double subtotalPrice;
     private double totalPrice;
+    private final double GST;
+    private final DecimalFormat DECIMALFORMAT;
 
     // Define the class constructor
     public ScanProductsController() {
         inventoryDB = InventoryDatabase.getInstance();
         inventoryDB.initialiseInventoryCollection();
         this.scannedBarcodedProducts = new ArrayList<>();
+        this.subtotalPrice = 0.0;
+        this.totalPrice = 0.0;
+        this.GST = 0.05;
+        this.DECIMALFORMAT  = new DecimalFormat("#.00");
     }
 
     // Scan the upc of the product
@@ -44,16 +51,26 @@ public class ScanProductsController {
         System.out.println(barcodedProduct.getShoeSize());
     }
 
+    // Update the subtotal price while scanning products
     private void updateSubtotalPrice() {
         this.subtotalPrice += barcodedProduct.getPrice();
-        System.out.println("Current subtotal: $ " + subtotalPrice);
+
+        // Testing purposes
+        System.out.println("Current subtotal: $ " + this.subtotalPrice);
+    }
+
+    // Calculate the total price with taxes
+    private void calculateTotalPrice() {
+        this.totalPrice = (this.subtotalPrice * this.GST) + this.subtotalPrice;
+        this.totalPrice = Double.parseDouble(DECIMALFORMAT.format(totalPrice));
+        System.out.println("Total amount: $" + this.totalPrice);
     }
 
     // Main method which deals with scanning products
     public void scanBarcodeProduct() {
 
-//        // 1. Scan the upc on the product
-//        scanUPC();
+        // 1. Scan the upc on the product
+        // scanUPC();
 
         // 2. Check if upc is in database;
         // a. If yes, add product to scannedProducts arraylist
@@ -61,6 +78,7 @@ public class ScanProductsController {
         if(inventoryDB.isProductUPCInDB(scannedUPC)) {
             addBarcodedProducts();
             updateSubtotalPrice();
+            calculateTotalPrice();
         } else {
             System.err.println("Scanned upc cannot be found in the inventory database.");
         }
@@ -74,7 +92,6 @@ public class ScanProductsController {
     public static void main(String[] args) {
         ScanProductsController scanProductsController = new ScanProductsController();
 
-
         scanProductsController.scanUPC();
         scanProductsController.scanBarcodeProduct();
 
@@ -83,7 +100,5 @@ public class ScanProductsController {
 
         scanProductsController.scanUPC();
         scanProductsController.scanBarcodeProduct();
-
-        System.out.println(scanProductsController.getScannedProducts());
     }
 }
