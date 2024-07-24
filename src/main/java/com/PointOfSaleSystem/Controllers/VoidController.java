44b5c8290b0769be81ff1e003db8a9a4621311d9
Soila Pertet <nicolepertet@gmail.com>
2024-07-24 14:53:51 -0600

    Define functionality to remove an item from the scanned products

--- src/main/java/com/PointOfSaleSystem/Controllers/VoidController.java
+++ src/main/java/com/PointOfSaleSystem/Controllers/VoidController.java
@@ -1,17 +1,43 @@
 package com.PointOfSaleSystem.Controllers;
 
+import com.PointOfSaleSystem.CentralPOSFacade.CentralPointOfSalesFacade;
 import com.PointOfSaleSystem.ProductLogic.BarcodedProduct;
+
+import java.util.List;
 
 public class VoidController {
 
     // Define instance variables
-    private BarcodedProduct productToVoid;
+    private CentralPointOfSalesFacade centralPOSFacade;
+    private long productUPCToVoid;
+    private BarcodedProduct voidedProduct;
+    private List<BarcodedProduct> scannedProducts;
 
     // Define the class constructor
-    public VoidController() { }
+    public VoidController() {
+        centralPOSFacade = CentralPointOfSalesFacade.getCentralPOSFacade();
+    }
+
+    // Remove specified product from scanned products ("cart") using its upc
+    public void voidItem(long upc) {
+
+        // initialise the product we want to void
+        this.productUPCToVoid = upc;
+
+        // initialise the already scanned products
+        scannedProducts = centralPOSFacade.getScanProductsController().getScannedProducts();
+
+        // loop through scanned products, look for and remove the product
+        for(BarcodedProduct scannedProduct : scannedProducts) {
+            if(productUPCToVoid == scannedProduct.getProductUPC()) {
+                this.voidedProduct = scannedProduct;
+                scannedProducts.remove(scannedProduct);
+            }
+        }
+    }
 
     // Define getter methods
-    public BarcodedProduct getProductToVoid() {
-        return productToVoid;
+    public BarcodedProduct getVoidedProduct() {
+        return voidedProduct;
     }
 }
