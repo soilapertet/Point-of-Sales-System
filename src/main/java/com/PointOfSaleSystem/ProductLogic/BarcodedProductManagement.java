9337b82f2debb8700eda72762bd0cb143dc7b33a
Soila Pertet <nicolepertet@gmail.com>
2024-07-29 14:40:06 -0600

    Initialise BarcodedProductManagement class

--- src/main/java/com/PointOfSaleSystem/ProductLogic/BarcodedProductManagement.java
+++ src/main/java/com/PointOfSaleSystem/ProductLogic/BarcodedProductManagement.java
@@ -4,7 +4,15 @@ import com.PointOfSaleSystem.CentralPOSFacade.CentralPointOfSalesFacade;
 
 public class BarcodedProductManagement  extends CentralPointOfSalesFacade {
 
+    // Define instance variables
+    private boolean discountApplied;
+    private double discountedPrice;
+
     public BarcodedProductManagement(CentralPointOfSalesFacade facade) {
         super(facade);
     }
+
+    // Define getter method
+    public double getDiscountedPrice() { return discountedPrice; }
+    public boolean getDiscountApplied() { return discountApplied; }
 }
