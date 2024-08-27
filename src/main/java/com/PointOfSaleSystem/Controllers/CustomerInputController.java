38a1392177b7f3d64bb9b53e8914008370095698
Soila Pertet <nicolepertet@gmail.com>
2024-08-27 11:11:11 -0600

    Add functionality for a staff purchase

--- src/main/java/com/PointOfSaleSystem/Controllers/CustomerInputController.java
+++ src/main/java/com/PointOfSaleSystem/Controllers/CustomerInputController.java
@@ -2,6 +2,7 @@ package com.PointOfSaleSystem.Controllers;
 
 import com.PointOfSaleSystem.CentralPOSLogic.CentralPointOfSalesController;
 import com.PointOfSaleSystem.StoreDatabase.CustomerDatabase;
+import com.PointOfSaleSystem.StoreDatabase.EmployeeDatabase;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Updates;
@@ -16,13 +17,16 @@ public class CustomerInputController extends CentralPointOfSalesController {
 
     // Define instance variables
     private CustomerDatabase customerDB;
+    private EmployeeDatabase employeeDB;
     private ObjectId uniqueID;
     private String customerFirstName;
     private String customerLastName;
+    private String employeeName;
     private long phoneNumber;
     private String emailAddress;
     private int membershipID;
     private boolean guestMode;
+    private boolean staffPurchase;
 
     private String inputEmail;
     private String firstNameInput;
@@ -38,7 +42,13 @@ public class CustomerInputController extends CentralPointOfSalesController {
         // Connect to store database and initialise "customer_account" collection
         customerDB = CustomerDatabase.getInstance();
         customerDB.initialiseCustomersCollection();
+
+        // Connect to store database and initialise "employees" collection
+        employeeDB = EmployeeDatabase.getInstance();
+        employeeDB.initialiseEmployeesCollection();
+
         this.guestMode = true;
+        this.staffPurchase = false;
     }
 
     public void checkForCustomerAccount(long inputPhoneNumber) throws Exception {
@@ -56,24 +66,6 @@ public class CustomerInputController extends CentralPointOfSalesController {
             setCustomerInfo();
         } else {
             throw new Exception ("Customer account could not be found with the provided phone number.");
-        }
-    }
-
-    public void checkForCustomerAccount(int inputMembershipID) throws Exception {
-
-        boolean isCustomerAMember;
-
-        // Initialise input details
-        this.inputMembershipID = inputMembershipID;
-
-        // Check if customer has an account
-        isCustomerAMember = hasACustomerAccount();
-
-        // Set customer details if they have a membership
-        if(isCustomerAMember) {
-            setCustomerInfo();
-        } else {
-            throw new Exception("Customer account could not be found with the provided membership ID.");
         }
     }
 
@@ -114,6 +106,24 @@ public class CustomerInputController extends CentralPointOfSalesController {
         }
     }
 
+    public void checkForCustomerAccount(int inputMembershipID) throws Exception {
+
+        boolean isAStaffMember;
+
+        // Initialise input details
+        this.inputMembershipID = inputMembershipID;
+
+        // Check if provided membershipID is an employeeID
+        isAStaffMember = employeeDB.isInEmployeeDatabase(inputMembershipID);
+
+        // Check if customer is a staff member
+        if(isAStaffMember) {
+             setEmployeeInfo();
+        } else {
+             initialiseNonStaffPurchaseSession();
+        }
+    }
+
     // Define a method to check if the user has a customer account
     private boolean hasACustomerAccount() {
 
@@ -126,6 +136,22 @@ public class CustomerInputController extends CentralPointOfSalesController {
         } else {
             return customerDB.isInCustomerDatabase(firstNameInput, lastNameInput);
         }
+    }
+
+    // Initialise a non-staff purchase session
+    private void initialiseNonStaffPurchaseSession() throws Exception {
+
+        boolean isCustomerAMember;
+
+        isCustomerAMember = hasACustomerAccount();
+
+        // Set customer details if they have a membership
+        if(isCustomerAMember) {
+            setCustomerInfo();
+        } else {
+            throw new Exception("Customer account could not be found with the provided membership ID.");
+        }
+
     }
 
     // Set the customer info based on customer details in customer DB
@@ -152,9 +178,20 @@ public class CustomerInputController extends CentralPointOfSalesController {
         this.phoneNumber = matchingDocs.first().getLong("phoneNumber");
         this.emailAddress = matchingDocs.first().getString("emailAddress");
         this.guestMode = false;
+    }
 
-        System.out.println("Customer info has been set for transaction");
+    private void setEmployeeInfo() {
 
+        Bson filter; FindIterable<Document> matchingDocs;
+
+        filter = Filters.eq("employeeId", inputMembershipID);
+        matchingDocs = employeeDB.getEmployeesCollection().find(filter);
+        this.uniqueID = matchingDocs.first().getObjectId("_id");
+        this.membershipID = matchingDocs.first().getInteger("employeeID");
+        this.employeeName = matchingDocs.first().getString("employeeName");
+
+        this.staffPurchase = true;
+        this.guestMode = false;
     }
 
     // Create a customer account based on user input
